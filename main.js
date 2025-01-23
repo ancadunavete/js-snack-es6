6061a5b39ab3e939bdf18723317ef1e0090ba104
ancadunavete <anca.dnvt@gmail.com>
2025-01-23 18:48:12 +0100

    add: js code

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
 Snack 3 (Bonus)
 Scrivere una funzione che accetti tre argomenti, un array ( esempio: ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'] )e due numeri (a più piccolo di b).
 La funzione ritornerà un nuovo array con li elementi estratti dall'array di partenza che hanno la posizione compresa tra i due numeri (a e b).*/
@@ -22,7 +23,41 @@ const biciDaCorsa = [
     { nome: "Mountain Climber", peso: 6.5}
 ]
 
-for (let i = 0; i < biciDaCorsa.length; i++ ) {
+let biciPiuLeggera = biciDaCorsa[0]
+
+for (let i = 1; i < biciDaCorsa.length; i++ ) {
     let elemento = biciDaCorsa[i]
-    
+
+    if (elemento.peso < biciPiuLeggera.peso) {
+        biciPiuLeggera = elemento
+    }
+
 }
+
+console.log(biciPiuLeggera)
+
+
+//--
+
+
+const squadreCalcio = [
+{ nome: "Juventus", punti: 0, falliSubiti: 0 }, 
+{ nome: "Milan", punti: 0, falliSubiti: 0 },
+{ nome: "Inter", punti: 0, falliSubiti: 0 }, 
+{ nome: "Napoli", punti: 0, falliSubiti: 0 }, 
+{ nome: "Roma", punti: 0, falliSubiti: 0 }
+];
+
+
+function numRandom(min, max){
+return Math.floor(Math.random() * max) + min;
+}
+
+for (const index in squadreCalcio) {
+let elemento = squadreCalcio[index]
+elemento.punti = numRandom(1, 50) 
+elemento.falliSubiti = numRandom(1, 50)
+}
+
+console.log(squadreCalcio )
+
